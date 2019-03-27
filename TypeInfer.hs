712f4e5a2f4b367f03b0b3fe81a248c294abd9de
charles fauman <charlesfauman@gmail.com>
2019-03-27 14:03:36 -0400

    more

--- TypeInfer.hs
+++ TypeInfer.hs
@@ -9,19 +9,26 @@ module TypeInfer (inferType) where
 import Control.Exception
 import Data
 import Utils
-import Data.List
 
-unify_one :: (Type, Type) -> [(TVar, Type)]
-unify_one (TArrow a1 a2, TArrow b1 b2) = do
-	s1 <- unify_one (a1, b1)
-	s2 <- unify_one (b1, b2)
-	return (nub (concat s1 s2))
-unify_one ((TVar a) t) = [(a, t)]
-unify_one (t (TVar a)) = [(a, t)]
-unify_one ((TBase a) (TBase b)) = [ ]
 
-unify :: Constraints -> Subst
-unify constraints = nub (concat (map unify_one constraints))
+unify_one :: (Type, Type) -> Subst
+unify_one (tau, (TVar t)) =
+	if (occursIn (TVar t) tau)
+	then throw TypeCircularity
+	else [(t, tau)]
+unify_one ((TVar t), tau) = 
+	if (occursIn (TVar t) tau)
+	then throw TypeCircularity
+	else [(t, tau)]
+unify_one ((TBase a), (TBase b)) =
+	if a == b
+	then [ ]
+	else throw (TypeMismatch (TBase a) (TBase b))
+unify_one ((TArrow t11 t12), (TArrow t21 t22)) =
+	let s1 = unify_one t11 t21
+		s2 = unify_one (applySubst t12 s1) (applySubst t22 s1)
+	in (s2 ++ s1)
+unify_one (tau_1, tau_2) = throw (TypeMismatch tau_1 tau_2)
 
 {- Performs type inference. -}
 inferTypes :: ConstraintEnv -> Exp -> (ConstraintEnv, Type)
