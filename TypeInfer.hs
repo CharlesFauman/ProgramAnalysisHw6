bfcc2b41876d4007608db7afe33c68e3061068bb
charles fauman <charlesfauman@gmail.com>
2019-03-28 08:55:16 -0400

    more

--- TypeInfer.hs
+++ TypeInfer.hs
@@ -30,18 +30,36 @@ unify_one ((TArrow t11 t12), (TArrow t21 t22)) =
     in (s2 ++ s1)
 unify_one (tau_1, tau_2) = throw (TypeMismatch tau_1 tau_2)
 
+
+applySubsOnGamma :: Subst -> ConstraintEnv -> ConstraintEnv
+applySubsOnGamma subs cenv =
+
 {- Performs type inference. -}
-inferTypes :: ConstraintEnv -> Exp -> (ConstraintEnv, Type)
+inferTypes :: ConstraintEnv -> Exp -> (ConstraintEnv, Subst, Type)
 inferTypes cenv (EVar var) = throw ToImplement
 inferTypes cenv (ELambda v body) = throw ToImplement
 inferTypes cenv (EApp fn arg) = throw ToImplement
-inferTypes cenv (ECond pred tbody fbody) = throw ToImplement
-inferTypes cenv (EPlus op1 op2) = throw ToImplement
+inferTypes cenv (ECond pred tbody fbody) =
+    let (cenv1, s1, t1) = (inferTypes cenv pred)
+        s2 = (unify_one (t1, boolType)) ++ s1
+        (cenv2, s3, t2) = (inferTypes (applySubsOnGamma s2 cenv1) tbody)
+        s4 = s3 ++ s2
+        (cenv3, s5, t3) = (inferTypes (applySubsOnGamma s4 cenv2) fbody)
+        s6 = (unify_one (t2, t3)) ++ s5 ++ s4
+    in (cenv3, s6, t2)
+inferTypes cenv (EPlus op1 op2) =
+    let (cenv1, s1, t1) = (inferTypes cenv op1)
+        s2 = (unify_one (t1, intType)) ++ s1
+        (cenv2, s3, t2) = (inferTypes (applySubsOnGamma s2 cenv1) op2)
+        s4 = (unify_one (t2, intType)) ++ s3 ++ s2
+    in (cenv2, s4, intType)
 inferTypes cenv (EPrim (PNum _)) = throw ToImplement
 inferTypes cenv (EPrim (PBool _)) = throw ToImplement
 inferTypes cenv (ELet s body) = throw ToImplement
 
 {- Top-level type inference function. I will be calling it on Submitty. -}
 inferType :: Exp -> Type
-inferType exp = throw ToImplement
+inferType exp = 
+  let (s, t) = inferTypes [] exp
+  in t
 
