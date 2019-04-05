489120b305d4d8c2e86da3dc71e9fbdd28d84cd4
charles fauman <charlesfauman@gmail.com>
2019-04-04 23:24:35 -0400

    ok

--- TypeInfer.hs
+++ TypeInfer.hs
@@ -60,8 +60,7 @@ findInEnv ((head_var, head_type):tail) var =
   else findInEnv tail var
 findInEnv [] var = throw (UnboundVar var)
 
-manageStatement :: ConstraintEnv -> Stmt -> (ConstraintEnv, Subst)
-manageStatement cenv SEmpty = (cenv, [])
+{-
 manageStatement cenv (SAssign ident exp) =
   let (cenv1, s1, t1) = inferTypes cenv exp
       cenv2 = applySubsOnGamma s1 cenv1
@@ -71,6 +70,30 @@ manageStatement cenv (SAssign ident exp) =
                    , tenv = (tenv cenv2) ++ [(ident, t2)]
                  }
   in (cenv3, s1)
+-}
+
+createVarsForStatement :: ConstraintEnv -> Stmt -> ConstraintEnv
+createVarsForStatement cenv SEmpty = cenv
+createVarsForStatement cenv (SAssign ident exp) =
+  let (tv, cenv_pre) = newTVar cenv
+      cenv_mid = CEnv { constraints = constraints cenv_pre
+                   , var = var cenv_pre
+                   , tenv = (tenv cenv_pre) ++ [(ident, tv)]
+                 }
+  in cenv_mid
+createVarsForStatement cenv (SSeq stmt1 stmt2) =
+  let cenv1 = createVarsForStatement cenv stmt1
+  in createVarsForStatement cenv1 stmt2
+
+manageStatement :: ConstraintEnv -> Stmt -> (ConstraintEnv, Subst)
+manageStatement cenv SEmpty = (cenv, [])
+manageStatement cenv (SAssign ident exp) =
+  let (cenv1, s1, t1) = inferTypes cenv exp
+      cenv2 = applySubsOnGamma s1 cenv1
+      t2 = applySubst t1 s1
+      s2 = (unify_one ((findInEnv (tenv cenv2) ident), t2))
+      cenv3 = applySubsOnGamma s2 cenv2
+  in (cenv3, s2 ++ s1)
 manageStatement cenv (SSeq stmt1 stmt2) =
   let (cenv1, s1) = manageStatement cenv stmt1
   in manageStatement cenv1 stmt2
@@ -82,14 +105,18 @@ inferTypes cenv (EVar var) =
   in (cenv, [], t)
 inferTypes cenv (ELambda v body) =
   let (tv, cenv1) = newTVar cenv
-      (cenv2, s1, t1) = (inferTypes cenv1 body)
-  in (cenv2, s1, (applySubst (TArrow tv t1) s1) )
+      cenv2 = CEnv { constraints = constraints cenv1
+                   , var = var cenv1
+                   , tenv = (tenv cenv1) ++ [(v, tv)]
+                 }
+      (cenv3, s1, t1) = inferTypes cenv2 body
+  in (cenv3, s1, applySubst (TArrow tv t1) s1)
 inferTypes cenv (EApp fn arg) =
   let (tv, cenv1) = newTVar cenv
       (cenv2, s1, t1) = (inferTypes cenv1 fn)
       (cenv3, s2, t2) = (inferTypes (applySubsOnGamma s1 cenv2) arg)
-      s3 = (unify_one ((applySubst t1 s2), (TArrow t2 tv)))
-  in (cenv3, s3 ++ s2 ++ s1, applySubst tv s3)
+      s3 = (unify_one ((applySubst t1 (s2 ++ s1)), (TArrow t2 tv)))
+  in (cenv3, s3 ++ s2 ++ s1, applySubst tv (s3 ++ s2 ++ s1))
 inferTypes cenv (ECond pred tbody fbody) =
     let (cenv1, s1, t1) = (inferTypes cenv pred)
         s2 = (unify_one (t1, boolType)) ++ s1
@@ -107,8 +134,8 @@ inferTypes cenv (EPlus op1 op2) =
 inferTypes cenv (EPrim (PNum _)) = (cenv, [], intType)
 inferTypes cenv (EPrim (PBool _)) = (cenv, [], boolType)
 inferTypes cenv (ELet s body) = 
-  let (cenv1, s1) = manageStatement cenv s
-      (cenv2, s2, t2) = inferTypes cenv1 body
+  let (cenv1, s1) = manageStatement (createVarsForStatement cenv s) s
+      (cenv2, s2, t2) = inferTypes (applySubsOnGamma s1 cenv1) body
   in (cenv2, s2 ++ s1, t2)
 
 {- Top-level type inference function. I will be calling it on Submitty. -}
